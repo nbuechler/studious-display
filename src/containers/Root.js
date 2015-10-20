fcf489e3e9988004b32256e5bf6edb839c1c6bbe
nbuechler <natebuechler@gmail.com>
2015-10-19 23:14:04 -0400

    added new dependecnies

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -3,13 +3,27 @@ import { Provider } from 'react-redux';
 import configureStore from '../configureStore';
 import AsyncApp from './AsyncApp';
 
+import {IndexRoute, Route, Router} from 'react-router';
+import {
+    Home,
+    Login
+  } from '../containers';
+
+
 const store = configureStore();
 
 export default class Root extends Component {
   render() {
     return (
       <Provider store={store}>
-        {() => <AsyncApp />}
+        {
+          <Router>
+            <Route path="/" component={AsyncApp}>
+              <IndexRoute component={Home}/>
+              <Route path="login" component={Login}/>
+            </Route>
+          </Router>
+        }
       </Provider>
     );
   }
