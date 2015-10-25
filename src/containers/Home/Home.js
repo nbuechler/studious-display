8e6e30204d96688e484803f1963ca6ce2eb5fcb2
nbuechler <natebuechler@gmail.com>
2015-10-24 20:56:21 -0400

    view for login page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,11 +5,15 @@ import { Button, Jumbotron } from 'react-bootstrap';
 class Home extends Component {
   render() {
     return (
-      <Jumbotron style={{margin: '0%', padding: '5%', background: '#111 none repeat scroll 0% 0%'}}>
+      <Jumbotron style={{margin: '0%', padding: '10%', background: '#111 none repeat scroll 0% 0%'}}>
         <h2 style={{textAlign: 'right'}}>Studious Display</h2>
         <h5>Figure out something cool to say here about the display!</h5>
         <h5>Then write it here!</h5>
-        <Button bsStyle="success" >Great!</Button>
+        <Link to="/login">
+          <Button bsStyle="success" className="pull-right">
+            Great!
+          </Button>
+        </Link>
       </Jumbotron>
     );
   }
