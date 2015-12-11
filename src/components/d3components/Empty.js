3f8310a0e8709a36c52830609c1f921f01086714
nbuechler <natebuechler@gmail.com>
2015-12-10 21:31:52 -0500

    removed a comment

--- src/components/d3components/Empty.js
+++ src/components/d3components/Empty.js
@@ -9,7 +9,6 @@ export default class Empty extends React.Component {
   }
   render () {
     return (
-      // <text x={this.props.offset} y={this.props.availableHeight - this.props.height}>No data!</text>
       <text x={0} y={this.props.height/2} fontFamily="Lato" fontSize="20px" fill="white">Sorry, no data available!</text>
     );
   }
