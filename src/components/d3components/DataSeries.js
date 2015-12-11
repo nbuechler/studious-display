37269d2fac8a590511b08036f046a87c10bf1986
nbuechler <natebuechler@gmail.com>
2015-12-10 21:08:45 -0500

    added explanitiory comment

--- src/components/d3components/DataSeries.js
+++ src/components/d3components/DataSeries.js
@@ -22,8 +22,13 @@ export default class DataSeries extends React.Component {
       .rangeRoundBands([0, this.props.width], 0.05);
 
     var color = '#AAA';
-    //The return statement needs to know what chart to apply the DataSeries to.
 
+    /**
+     * The return statement needs to know what chart to apply the DataSeries to.
+     * And, here's the thing, this.props.chart actually gets defined in the
+     * React Component that calls DataSeries, where the defnition is, sort of
+     * like dependency injection.
+     */
     switch (this.props.chart) {
       case 'bar':
       var bars = _.map(this.props.data, function(point, i) {
