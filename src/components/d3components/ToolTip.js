ade96cf7f3745d48a586634e3f78d3fb9d8c8512
nbuechler <natebuechler@gmail.com>
2015-12-16 22:55:35 -0500

    centered text in tip

--- src/components/d3components/ToolTip.js
+++ src/components/d3components/ToolTip.js
@@ -8,33 +8,34 @@ export default class ToolTip extends React.Component {
     this.state = { };
   }
   render () {
-    var ttRectWidth= this.props.ttRectWidth,
-        ttRectHeight= this.props.ttRectHeight;
+    var ttRectWidth = this.props.ttRectWidth,
+        ttRectHeight = this.props.ttRectHeight,
+        scaleShift = this.props.offset + this.props.width/2;
 
     var textElement = '';
 
     if(this.props.mainText > 99999){
-      textElement = <text id={this.props.tipId} x={this.props.offset + ttRectWidth/2 }
+      textElement = <text id={this.props.tipId} x={scaleShift - ttRectWidth/2 + 8}
                           y={this.props.availableHeight/2}
                           fill={'lightGreen'}>{'0.' + Math.round(this.props.mainText/100000) + 'M'}</text>;
     } else if(this.props.mainText > 9999){
-      textElement = <text id={this.props.tipId} x={this.props.offset + ttRectWidth/2 }
+      textElement = <text id={this.props.tipId} x={scaleShift - ttRectWidth/2 + 10}
                           y={this.props.availableHeight/2}
                           fill={'white'}>{Math.round(this.props.mainText/1000) + 'K'}</text>;
     } else if(this.props.mainText > 999){
-      textElement = <text id={this.props.tipId} x={this.props.offset + ttRectWidth/2 + 5 }
+      textElement = <text id={this.props.tipId} x={scaleShift - ttRectWidth/2 + 15}
                           y={this.props.availableHeight/2}
                           fill={'white'}>{Math.round(this.props.mainText/1000) + 'K'}</text>;
     } else if(this.props.mainText > 99){
-      textElement = <text id={this.props.tipId} x={this.props.offset + ttRectWidth/2 }
+      textElement = <text id={this.props.tipId} x={scaleShift - ttRectWidth/2 + 10}
                           y={this.props.availableHeight/2}
                           fill={'white'}>{this.props.mainText}</text>;
     } else if (this.props.mainText > 9) {
-      textElement = <text id={this.props.tipId} x={this.props.offset + ttRectWidth/2 + 5 }
+      textElement = <text id={this.props.tipId} x={scaleShift - ttRectWidth/2 + 15}
                           y={this.props.availableHeight/2}
                           fill={'white'}>{this.props.mainText}</text>;
     } else {
-      textElement = <text id={this.props.tipId} x={this.props.offset + ttRectWidth/2 + 10 }
+      textElement = <text id={this.props.tipId} x={scaleShift - ttRectWidth/2 + 20}
                           y={this.props.availableHeight/2}
                           fill={'white'}>{this.props.mainText}</text>;
     }
