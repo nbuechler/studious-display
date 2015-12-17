f4a6e132866cd23a0160c80881c59d2081ace0b3
nbuechler <natebuechler@gmail.com>
2015-12-16 22:48:08 -0500

    scaling the numbers

--- src/components/d3components/ToolTip.js
+++ src/components/d3components/ToolTip.js
@@ -13,24 +13,36 @@ export default class ToolTip extends React.Component {
 
     var textElement = '';
 
-    if(this.props.mainText > 99){
+    if(this.props.mainText > 99999){
       textElement = <text id={this.props.tipId} x={this.props.offset + ttRectWidth/2 }
-                          y={this.props.availableHeight + this.props.buffers.top/2 - this.props.height + ttRectHeight/2}
+                          y={this.props.availableHeight/2}
+                          fill={'lightGreen'}>{'0.' + Math.round(this.props.mainText/100000) + 'M'}</text>;
+    } else if(this.props.mainText > 9999){
+      textElement = <text id={this.props.tipId} x={this.props.offset + ttRectWidth/2 }
+                          y={this.props.availableHeight/2}
+                          fill={'white'}>{Math.round(this.props.mainText/1000) + 'K'}</text>;
+    } else if(this.props.mainText > 999){
+      textElement = <text id={this.props.tipId} x={this.props.offset + ttRectWidth/2 + 5 }
+                          y={this.props.availableHeight/2}
+                          fill={'white'}>{Math.round(this.props.mainText/1000) + 'K'}</text>;
+    } else if(this.props.mainText > 99){
+      textElement = <text id={this.props.tipId} x={this.props.offset + ttRectWidth/2 }
+                          y={this.props.availableHeight/2}
                           fill={'white'}>{this.props.mainText}</text>;
     } else if (this.props.mainText > 9) {
       textElement = <text id={this.props.tipId} x={this.props.offset + ttRectWidth/2 + 5 }
-                          y={this.props.availableHeight + this.props.buffers.top/2 - this.props.height + ttRectHeight/2}
+                          y={this.props.availableHeight/2}
                           fill={'white'}>{this.props.mainText}</text>;
     } else {
       textElement = <text id={this.props.tipId} x={this.props.offset + ttRectWidth/2 + 10 }
-                          y={this.props.availableHeight + this.props.buffers.top/2 - this.props.height + ttRectHeight/2}
+                          y={this.props.availableHeight/2}
                           fill={'white'}>{this.props.mainText}</text>;
     }
 
     return (
       <g style={{visibility: '', fontWeight: 900, textAlign: 'center'}}>
         <rect width={ttRectWidth} height={ttRectHeight} fill="#111" opacity="0.9" stroke="black" strokeWidth="3px"
-              x={this.props.offset + this.props.width/2 - ttRectWidth/2} y={this.props.availableHeight + this.props.buffers.top/2 - this.props.height} >
+              x={this.props.offset + this.props.width/2 - ttRectWidth/2} y={this.props.availableHeight/2 - 30} >
         </rect>
         {textElement}
       </g>
