7a63e853829e7a0a33bd8e10bdb519922c9cc975
nbuechler <natebuechler@gmail.com>
2015-12-20 18:49:58 -0500

    updated title field

--- src/components/d3charts/BarChart.js
+++ src/components/d3charts/BarChart.js
@@ -20,7 +20,7 @@ export default class BarChart extends React.Component {
     return (
       <Wrapper border={this.props.border} borderWeight={this.props.borderWeight} width={this.props.width} height={this.props.height}>
         <DataSeries distinctColors={this.props.distinctColors} fillColors={this.props.fillColors}
-          chart={'bar'} modulus={this.props.modulus}
+          chart={'bar'} modulus={this.props.modulus} title={this.props.title}
           data={this.props.data} width={this.props.width} height={this.props.height}/>
       </Wrapper>
     );
