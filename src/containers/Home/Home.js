1114bbbc335848bff39b25bdc333b0a344a66771
nbuechler <natebuechler@gmail.com>
2016-02-04 23:57:16 -0500

    added a descirption

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -7,8 +7,8 @@ class Home extends Component {
     return (
       <Jumbotron style={{margin: '0%', padding: '10%', background: '#111 none repeat scroll 0% 0%'}}>
         <h2 style={{textAlign: 'right'}}>Studious Display</h2>
-        <h5>Figure out something cool to say here about the display!</h5>
-        <h5>Then write it here!</h5>
+        <h5>The introspective learning tool includes a variety of data visualizations.</h5>
+        <h5>Fantastic understandings about your log entries are synthesized to enhance your emotional intelligence.</h5>
         <Link to="/login">
           <Button bsStyle="success" className="pull-right">
             Great!
