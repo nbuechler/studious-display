e076589978d7c2e00faa002c3a05617d131c3a72
nbuechler <natebuechler@gmail.com>
2016-02-07 21:57:32 -0500

    added the calendar cell clicking ability

--- src/components/d3components/CalendarCell.js
+++ src/components/d3components/CalendarCell.js
@@ -9,6 +9,13 @@ export default class Bar extends React.Component {
     this.state = {};
   }
 
+  _handleClick(d) {
+    console.log(d);
+    console.log(this);
+    localStorage.setItem('reflectionDate', d.date);
+    d.openModal(); //Opens the modal information from the parent... in LogDisplay.js
+  }
+
   render () {
 
     var monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
@@ -20,7 +27,8 @@ export default class Bar extends React.Component {
         width={this.props.width - 4} height={this.props.height - 4}
         x={this.props.x + 2} y={this.props.y + 2}
         rx="5" ry="5"
-        style={{stroke: '#111', strokeWidth: '2px'}}>
+        style={{stroke: '#111', strokeWidth: '2px'}}
+        onClick={this._handleClick.bind(this, this.props)}>
         <title><b>{this.props.logCount}</b> logs on {monthNames[this.props.month].substring(0,3)} {this.props.day}, {this.props.year + 1900}</title>
       </rect>
     );
